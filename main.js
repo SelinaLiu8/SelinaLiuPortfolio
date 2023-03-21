4d58770c938b15c13d25f2f44aea657866482493
selinal <selnal@umich.edu>
2023-03-21 16:53:25 -0400

    project pages

--- main.js
+++ main.js
@@ -24,3 +24,49 @@ if (window.matchMedia("(max-width: 964px)").matches) {
         toggleNav();
     }))
 }
+
+// Dots background
+// const container = document.querySelector('.dots-container');
+// const about = document.getElementById("about");
+// const dots = [];
+// let dotWidth;
+// const minWidth = 0.7;
+
+// for (let i = 0; i < 15; i++) {
+//   const dot = document.createElement('div');
+//   dot.classList.add('dot');
+//   dotWidth = Math.floor(Math.random() * 150) + 5;
+
+//   while (dotWidth < minWidth) {
+//     dotWidth = Math.floor(Math.random() * 150) + 5;
+//   }
+
+//   dot.style.width = `${dotWidth}px`;
+//   dot.style.height = `${dotWidth}px`;
+//   dot.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
+//   dot.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+  
+//   // Check for overlaps and re-position the dot if necessary
+//   let overlap = true;
+//   while (overlap) {
+//     overlap = false;
+//     for (const otherDot of dots) {
+//       if (getDistance(dot, otherDot) < (dot.offsetWidth + otherDot.offsetWidth) / 2) {
+//         overlap = true;
+//         dot.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
+//         dot.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+//         break;
+//       }
+//     }
+//   }
+  
+//   dots.push(dot);
+//   container.appendChild(dot);
+// }
+
+// function getDistance(dot1, dot2) {
+//   const xDiff = dot1.offsetLeft - dot2.offsetLeft;
+//   const yDiff = dot1.offsetTop - dot2.offsetTop;
+//   return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+// }
+
